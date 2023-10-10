6e329735114550049c3d457dbda0390425f0bb0c
cleiton lahm <cleijllahm@gmail.com>
2023-10-10 11:57:18 -0300

    iniciando configuracoes de rotas com redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,29 @@
 import React from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
 import EstiloGlobal, { Container } from './styles'
-import BarraLateral from './containers/BarraLateral'
-import ListaDeTarefas from './containers/ListaDeTarefas'
 import { Provider } from 'react-redux'
 
 import store from './store'
+import Home from './pages/Home'
+
+const rotas = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home></Home>
+  },
+  {
+    path: '/novo',
+    element: <h1>Nova tarefa</h1>
+  }
+])
 
 function App() {
   return (
     <Provider store={store}>
       <EstiloGlobal></EstiloGlobal>
       <Container>
-        <BarraLateral></BarraLateral>
-        <ListaDeTarefas></ListaDeTarefas>
+        <RouterProvider router={rotas}></RouterProvider>
       </Container>
     </Provider>
   )
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import BarraLateral from '../../containers/BarraLateral'
+import ListaDeTarefas from '../../containers/ListaDeTarefas'
+
+const Home = () => {
+  return (
+    <>
+      <BarraLateral></BarraLateral>
+      <ListaDeTarefas></ListaDeTarefas>
+    </>
+  )
+}
+
+export default Home
